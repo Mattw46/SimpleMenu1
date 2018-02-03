7523ccb43a219714ff08994ad2bfaf8bfe575db4
Mattw46 <matthew_w46@yahoo.com.au>
2018-02-03 16:38:36 +1100

    second commit: update README and add main class

---
+++ Prog1Ass1.java
@@ -0,0 +1,21 @@
+/*
+ * Implementing Assignment 1 for RMIT Programming 1 Course
+ * Implementing using current knowledge with aim to use 
+ * best practice. First major project in Java in a while.
+ */
+package prog1ass1;
+
+/**
+ *
+ * @author Matt W
+ */
+public class Prog1Ass1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
