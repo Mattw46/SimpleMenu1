02834b87d47fd9f9324677113f934f77d6f6d18b
Mattw46 <matthew_w46@yahoo.com.au>
2018-02-14 14:38:41 +1100

    implemented basic stamp duty and added tests

---
+++ StampDuty/StampDutyCalculator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog1ass1.StampDuty;
+
+
+/**
+ *
+ * @author Matt W
+ */
+public class StampDutyCalculator {
+    
+    public void getStampDuty() {
+        System.out.println("Enter amount");
+    }
+    
+    public void getStampDuty(double amount) {
+        calculate(amount);
+    }
+    
+    private void calculate(double amt) {
+        double stampDuty;
+        
+        if (amt <= 20000) {
+            stampDuty = amt * 0.014;
+        }
+        else if (amt > 20000 && amt <= 115000) {
+            stampDuty = (20000 * 0.014) + ((amt - 20000)* 0.024);
+        }
+        else if (amt > 115000 && amt <= 870000) {
+            stampDuty = (20000 * 0.014) + ((115000 - 20000)* 0.024) + 
+                    ((amt - 115000)* 0.06);
+        }
+        else {
+            stampDuty = amt * 0.055;
+        }
+        
+        System.out.println("Stamp duty on " + amt + " is " + stampDuty);
+    }
+}
--- Tests.java
+++ Tests.java
@@ -4,7 +4,9 @@
  */
 package prog1ass1;
 
+import java.math.BigDecimal;
 import prog1ass1.CharacterPattern.CharacterPattern;
+import prog1ass1.StampDuty.StampDutyCalculator;
 
 /**
  *
@@ -22,8 +24,18 @@ public class Tests {
         System.out.println("printing T 3 times\n");
         pattern.PrintPattern(3, 'T');
         System.out.println();
-        System.out.println("calling print without arguments");
-        pattern.PrintPattern();
+        //System.out.println("calling print without arguments");
+        //pattern.PrintPattern();
+        System.out.println("Test calculate stamp duty:");
+        StampDutyCalculator sdc = new StampDutyCalculator();
+        sdc.getStampDuty(18000);
+        System.out.println();
+        sdc.getStampDuty(50000);
+        System.out.println();
+        sdc.getStampDuty(400000);
+        System.out.println();
+        sdc.getStampDuty(89600);
+        System.out.println();
         
         System.out.println("**** Testing is complete ****");
     }
