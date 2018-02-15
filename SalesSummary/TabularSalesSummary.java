735d6d44246835fbef055f37afc0568d8ef6d8ed
Mattw46 <matthew_w46@yahoo.com.au>
2018-02-15 15:02:40 +1100

    Commit changes so far to TabularSalesSummary - still incomplete

--- SalesSummary/TabularSalesSummary.java
+++ SalesSummary/TabularSalesSummary.java
@@ -5,6 +5,7 @@
  */
 package prog1ass1.SalesSummary;
 
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -22,11 +23,42 @@ public class TabularSalesSummary {
         Sale s = new Sale();
         Scanner sc = new Scanner(System.in);
         // prompt for values and set value in sale instance.
+        System.out.println("");
+        System.out.print("Product code: ");
         s.setProductCode(sc.nextLine());
-        return null;
+        System.out.print("\nProduct Description: ");
+        s.setDescription(sc.nextLine());
+        System.out.print("\nUnit Price: $");
+        s.setUnitPrice(sc.nextDouble());
+        System.out.print("\nQuantity: ");
+        s.setQuantity(sc.nextInt());
+        System.out.print("\nAdditional Discount: $");
+        s.setExtraDiscount(sc.nextDouble());
+        System.out.print("\nAuthorising sales person: ");
+        s.setSalesPerson(sc.nextLine());
+        return s;
     }
     
     private void displaySalesSummary(Sale s) {
-    
+        Date date = new Date();
+        
+       System.out.println("------------------------" +
+               " Hardly Normal Proof of Sale " + 
+               "-----------------------\n");
+       System.out.println("Prod. Code    Product Description" +
+               "              Unit Price    Qty   Total\n");
+       System.out.println("-----------   ------------------------------" +
+               "   -----------    ----  -----------");
+       System.out.printf("%-10s    %-30s    $%10.2f   %4d" + "  $%10.2f"
+               ,s.getProductCode(),s.getDescription(),s.getUnitPrice()
+               ,s.getQuantity(),550.00);
+       System.out.println("Sub-total:  $"); //make printf
+       System.out.println("Multi-Buy discount:  $");
+       System.out.println("Additional Discount (" + s.getSalesPerson() +
+               "):  $");
+       System.out.println("----------");
+       System.out.println("Total:  $");
+       System.out.println("GST Paid:  $");
+       System.out.println("Date: " + date.toString());
     }
 }
